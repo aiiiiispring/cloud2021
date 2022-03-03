0cff25142f33b05272cb81188d70c620d31eb1d8
aiiiiispring <1554454156@qq.com>
2022-03-03 16:49:15 +0800

    修改

---
+++ cloud-config-center3344/src/main/java/com/atguigu/cloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.atguigu.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author aiiiiii
+ * @create 2022/3/3 16:13
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
